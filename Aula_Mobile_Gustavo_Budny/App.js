2f1bb2bc8701ac035ebde7ce1f6a7b2fd2c7ca83
Gustavo Budny <gustavosouza1317@gmail.com>
2026-03-24 16:41:19 -0300

    aaaa

--- Aula_Mobile_Gustavo_Budny/App.js
+++ Aula_Mobile_Gustavo_Budny/App.js
@@ -1,12 +1,53 @@
-
-import Logar from './components/tela_nova';
+import {View, Text, StyleSheet, Image, ScrollView} from 'react-native'
+import { Button, Card } from 'react-native-paper';
+import { drivers } from './data/drivers';
 
 export default function App() {
+
+  //const motoristas = drivers[1]
+
   return (
-    
-      <Logar></Logar>
+    <ScrollView style={styles.}>
+    <View style={styles.container}>
+     {drivers.map{(motoristas) => {
+      <Card>
+        <Card.Cover source={{uri: motoristas.image}}/>
+        <Card.Content style={styles.cards}>
+
+        <Text style={styles.txt}>{motoristas.name}</Text>
+        <Text style={styles.txt}>{motoristas.description}</Text>
+        <Text style={styles.txt}>{motoristas.team}</Text>
+        <Text style={styles.txt}>{motoristas.country}</Text>
+        <Text style={styles.txt}>{'⭐'.repeat(motoristas.stars)}</Text>
+
+       </Card.Content>
+     </Card>
+     }}}
+  </View>
+  </ScrollView>
  
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  imagem : {
+    width: 300,
+    height: 300
+  },
+
+  cards : {
+    alignItems: 'center',
+    textAlign: 'center'
+  },
+
+  txt: {
+    fontSize: 20,
+    textAlign: 'center'
+  }
+})
 
--- Aula_Mobile_Gustavo_Budny/components/Profile.js
+++ Aula_Mobile_Gustavo_Budny/components/Profile.js
@@ -1,26 +1,69 @@
-import {View, Text, Image} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native'
+import { useState } from 'react';
+import { Button, Card } from 'react-native-paper';
 
-export function Person(){
-    return(
-        <View>
-            <Text>Meu componente pessoa</Text>
-        </View>
-    )
+export default function Profaaa() {
+  
+  const [mensagem, setMensagem] = useState('OLá')
+  const [somaUm, setSomaUm] = useState(0)
+
+  function TrocaMsg() {
+    if (mensagem == 'Olá'){
+    setMensagem('Oi, trocamos de mensagem!')
+    } else {
+      setMensagem('Olá')
+    }
+  }
+
+  function Contador(){
+    setSomaUm(somaUm + 1)
+  }
+
+  function Diminuir() {
+    if(somaUm > 0) {
+    setSomaUm(somaUm - 1)
+    } 
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.txt}>{mensagem}</Text>
+
+      <Button buttonColor='#d61d4eff' mode='contained' onPress={TrocaMsg}>
+          pecione para trocar mensagem
+      </Button>
+
+      <Text style={styles.txt}>{somaUm}</Text>
+      <View style={styles.botao}>
+
+        <Button buttonColor='#d61dc6ff' mode='contained' onPress={Contador}>
+          pecione para aumentar
+        </Button>
+
+        <Button buttonColor='#dc7208ff' mode='contained' onPress={Diminuir}>
+          pecione para diminuir
+        </Button>
+
+
+      </View>
+    </View>
+ 
+  );
 }
 
-export function Gustavo(){
-    return(
-        <View>
-            <Text>minha imagem</Text>
-            <Image source={require('../Sources/viniciussssssss.jpg')}/>
-        </View>
-    )
-}
-
-export default function Profile() {
-    return(
-        <View>
-            <Text>Meu primeiro coponente</Text>
-        </View>
-    )
-}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  txt : {
+    fontSize: 30,
+    color: '#c10d0dff'
+  },
+  botao : {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-around'
+  }
+})
